079130bfe4cd9456dd6fe4f956433018cfb86061
endribrahja <endri.brahja@marconirovereto.it>
2026-01-29 11:04:22 +0000

    Implement print, size, and merge methods in DoublyLinkedList class

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -21,6 +21,7 @@ public class DoublyLinkedList {
         }
     }
 
+
     // wrapper
     private Node getRec(Node cursor, int index) {
 
@@ -44,22 +45,56 @@ public class DoublyLinkedList {
     }
 
     // ------------ INSERT YOUR CODE HERE ----------------
-
+//QUA INIZIA PRINT
     public void print(boolean forward) {
-
+        if(forward){
+            Node cursor = head;
+            while (cursor != null){
+                System.out.print(cursor.getData());
+                cursor = cursor.getNext();
+            }
+        }else{
+            Node cursor = tail;
+            while(cursor != null){
+                System.out.print(cursor.getData());
+                cursor = cursor.getPrev();
+            }
+        }
+        System.out.println();
     }
-
+    
+//QUA INIZIA SIZE
     public int size() {
-
-        return 0;
+        int count = 0;
+        Node cursor = head;
+        while(cursor != null){
+            count++;
+            cursor = cursor.getNext();
+        }
+        return count;
     }
 
+//QUA INIZIA MERGE
     public void merge(DoublyLinkedList otherList) {
+        if(otherList.head == null){
+           return; 
+        }
+
+        if(this.head == null) {
+            this.head = otherList.head;
+            this.tail = otherList.tail;
+        }else{
+            this.tail.setNext(otherList.head);
+            otherList.head.setPrev(this.tail);
+            this.tail = otherList.tail;
 
     }
+
 
     public String slice(int start, int end) {
+        Node cursor = head;
 
+        int count = 0;
         return null;
     }
 
@@ -70,7 +105,23 @@ public class DoublyLinkedList {
     }
 
     public void shift(int amount) {
+        if (head == null || amount <= 0){
+            return;
+        }
 
+        Node cursor = head;
+        int count = 0;
+        while(cursor != null){
+            cursor = cursor.getNext();
+            count++;
+        }
+        if(cursor == null) return;
+
+        int n = size();
+        amount = amount % n;
+
+
+      
     }
 
     public void trim(int newSize) {
